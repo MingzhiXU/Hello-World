3caf9d7d6f31bc3a4cbc923e3f07494abaf65971
Mingzhi Xu <xumingzhi94@gmail.com>
2024-01-26 13:12:32 +0800

    correct some grammer mistakes

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,6 @@
 
 def helloworld():
-    print("what is a fork, it is too difficult,i am going crazy, i want to go to home and sleep.")
+    print("what is a fork, it is too difficult,i am going to be crazy, i want to go home and sleep.")
 
 
 if __name__ == "__main__":
